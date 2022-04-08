b0ab89fd3f2ee0ffd5ba011819fb6adc5abd1021
Naser Mirzaei <nasermirzaei89@gmail.com>
2022-04-08 22:56:07 +0430

    Add NewWithLogger to output logger

--- output/logger.go
+++ output/logger.go
@@ -2,13 +2,22 @@ package output
 
 import (
 	"fmt"
-	"github.com/nasermirzaei89/problem"
 	"log"
+
+	"github.com/nasermirzaei89/problem"
 )
 
 func New() problem.Logger {
 	return func(err error) string {
 		log.Println(fmt.Sprintf("%+v", err))
+
+		return ""
+	}
+}
+
+func NewWithLogger(l *log.Logger) problem.Logger {
+	return func(err error) string {
+		l.Println(fmt.Sprintf("%+v", err))
 
 		return ""
 	}
